28f85be17b465ac092284bf81d70fa5609f39935
carlosga <carlos.guzman.1@outlook.es>
2016-03-17 23:12:33 +0100

    · Reset password

--- connection/Program.cs
+++ connection/Program.cs
@@ -12,7 +12,7 @@ namespace ConsoleApplication
             csb.DataSource      = "localhost";
             csb.InitialCatalog  = "chronos";
             csb.UserID          = "chronos";
-            csb.Password        = "::pelanas.2016::";
+            csb.Password        = "::pgsql::";
             csb.PortNumber      = 5432;
             csb.Ssl             = false;
             csb.Pooling         = false;
