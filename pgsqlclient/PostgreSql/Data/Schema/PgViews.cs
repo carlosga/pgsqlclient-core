b603b693851385117501c1e9aab06940304b3f37
carlosga <carlos.guzman.1@outlook.es>
2016-03-17 20:11:02 +0100

    · Updated line endings & license headers that were unchanged after the code reformatting done using the dotnet CodeFormatter tool

--- pgsqlclient/PostgreSql/Data/Schema/PgViews.cs
+++ pgsqlclient/PostgreSql/Data/Schema/PgViews.cs
@@ -1,6 +1,4 @@
-// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
+// Copyright (c) Carlos Guzmán Álvarez. All rights reserved.
 // Licensed under the Initial Developer's Public License Version 1.0. See LICENSE file in the project root for full license information.
 
 using PostgreSql.Data.PostgreSqlClient;
