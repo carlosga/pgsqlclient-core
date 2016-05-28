ff7cbfb36fa4623152f407f6d51f55c9c0c76711
carlosga <carlos.guzman.1@outlook.es>
2016-05-28 10:51:04 +0200

    · Updated project.json

--- src/PostgreSql.Data.SqlClient/src/PostgreSql/Data/Common/RetryOperation.cs
+++ src/PostgreSql.Data.SqlClient/src/PostgreSql/Data/Common/RetryOperation.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Carlos Guzmán Álvarez. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
-using System.Linq;
 using System.Threading;
 
 namespace System.Data.Common
