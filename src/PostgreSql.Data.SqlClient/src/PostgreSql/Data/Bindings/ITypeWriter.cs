4a11683258f5a85406394d2dd1b0960980608438
carlosga <carlos.guzman.1@outlook.es>
2016-07-02 00:44:01 +0200

    · USing cleanup

--- src/PostgreSql.Data.SqlClient/src/PostgreSql/Data/Bindings/ITypeWriter.cs
+++ src/PostgreSql.Data.SqlClient/src/PostgreSql/Data/Bindings/ITypeWriter.cs
@@ -1,9 +1,6 @@
 // Copyright (c) Carlos Guzmán Álvarez. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
-using System.Collections.Generic;
-
 namespace PostgreSql.Data.Bindings
 {
     public interface ITypeWriter
